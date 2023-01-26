c330076a0013b230532ffbfc8c82cd52dc1b5bbd
jovaslink <jovaslink@gmail.com>
2023-01-26 03:02:04 -0600

    AXIOS, CONEXIÓN ENTRE LA API NODE Y API PHP

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const {response,request}=require('express');
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { default: axios } = require('axios');
 
 const authLogin = async (req = request, res = response)=>{
     const {correo,password} = req.body;
@@ -36,9 +37,21 @@ const authLogin = async (req = request, res = response)=>{
     // asignar JWT
     const token = await generarJWT(usuario.id);
 
+    //PROVISIONAL AXIOS CONECCION CON BRIDGE 
+
+    const respuesta = await axios.post('http://localhost/bridge/bridge.php',{
+        name: 'HOLA DESDE JAM REST API NODE'
+      });
+      const proveedor= respuesta.data;
+
+     //PROVISIONAL AXIOS CONECCION CON BRIDGE 
+
+
     res.json({
         usuario,
-        token
+        token,
+        proveedor
+        
      });
 
    } 
