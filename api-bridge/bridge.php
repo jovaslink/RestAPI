499d994de8d2df07c80f3fb651e0021ffceb0a7c
jovaslink <jovaslink@gmail.com>
2023-01-26 00:24:28 -0600

    Bridge comunicando

--- api-bridge/bridge.php
+++ api-bridge/bridge.php
@@ -15,24 +15,27 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 $data = json_decode(file_get_contents("php://input"));
 
-if(!empty($data->name) && !empty($data->description) &&
+/* if(!empty($data->name) && !empty($data->description) &&
 !empty($data->price) && !empty($data->category_id) &&
-!empty($data->created)){
+!empty($data->created)){ */
+if($data->name){
 
-$items->name = $data->name;
+    echo json_encode(array("message" => $data->name));
+
+/* $items->name = $data->name;
 $items->description = $data->description;
 $items->price = $data->price;
 $items->category_id = $data->category_id; 
 $items->created = date('Y-m-d H:i:s'); 
-
-if($items->create()){ 
+ */
+/* if($items->create()){ 
 http_response_code(201); 
 echo json_encode(array("message" => "Item was created."));
 } else{ 
 http_response_code(503); 
 echo json_encode(array("message" => "Unable to create item."));
-}
-}else{ 
+} */
+} else{ 
 http_response_code(400); 
 echo json_encode(array("message" => "Unable to create item. Data is incomplete."));
 }
